e28bd7091208558a65176bfdd99cdf973be165b8
Your Name <you@example.com>
2023-11-13 15:02:25 +0800

    new

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <index msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import index from './components/my_index.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    index
+    HelloWorld
   }
 }
 </script>
