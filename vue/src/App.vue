6795b32da2421199370fe8442ff5bc2baa03f7dc
Your Name <you@example.com>
2023-11-13 14:29:09 +0800

    add new vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <index msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import index from './components/my_index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    index
   }
 }
 </script>
