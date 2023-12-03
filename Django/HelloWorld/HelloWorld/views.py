a4096e3162776ef79ee44019e8721d1e1471f9bf
Your Name <you@example.com>
2023-12-03 23:55:19 +0800

    add Django

---
+++ Django/HelloWorld/HelloWorld/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def runoob(request):
+    views_list = ["菜鸟教程","菜鸟教程1","菜鸟教程2","菜鸟教程3",]
+    return render(request, "runoob.html", {"views_list": views_list})
